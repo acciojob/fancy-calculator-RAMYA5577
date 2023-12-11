6a43d46d2770d558ee60bbe91048c89fffae7b89
RAMYA5577 <121097897+RAMYA5577@users.noreply.github.com>
2023-12-11 21:24:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+var var1=prompt("Enter first value");
+var1=var1*2;
+var var2=prompt("Enter second value");
+
+var result=parseInt(var1)+parseInt(var2);
+alert(`The final result is: ${result}`);
